80215bc93ac2b8d516c26833b28c9b76f457433e
Sarah <Sarahnsweet@gmail.com>
2022-02-07 20:59:40 -0800

    AddRoutes

--- code/app/home/views.py
+++ code/app/home/views.py
@@ -6,3 +6,15 @@ from app.home import home
 @home.route('/')
 def index():
     return render_template('index.html')
+@app.route("/login")
+def login():
+    return render_template("index.html")
+@app.route("/admin")
+def admin():
+    return render_template("index.html")
+@app.route("/mypage1")
+def mypage1():
+    return render_template("index.html")
+@app.route("/testnew")
+def testnew():
+    return render_template("new.html")
