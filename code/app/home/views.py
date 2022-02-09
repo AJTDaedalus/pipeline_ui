7ef37b0242e7b9b263cab41569a179cbebc88e63
Sarah <Sarahnsweet@gmail.com>
2022-02-08 21:29:28 -0800

    pages

--- code/app/home/views.py
+++ code/app/home/views.py
@@ -6,3 +6,15 @@ from app.home import home
 @home.route('/')
 def index():
     return render_template('home.html')
+@home.route('/login')
+def index():
+    return render_template('home.html')
+
+@home.route('/home')
+def index():
+    return render_template('home.html')
+
+@home.route('/admin')
+def index():
+    return render_template('home.html')
+
