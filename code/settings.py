f1f9d469d732e80d21656afefc47dd492440a530
AJTDaedalus <aaron.j.tyler@gmail.com>
2022-02-14 20:14:28 -0600

    Added back main.py and settings.py

---
+++ code/settings.py
@@ -0,0 +1,22 @@
+'''
+1Feb22 BIOT670 Group 6
+settings file for bioinformatics pipeline
+structure derived from github.com/corpsgeek/social-app-structure
+'''
+
+import os
+
+#Set the base directory
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+#Create superclass
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#Create development settings
+class DevelopmentSettings(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'dev-data.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
