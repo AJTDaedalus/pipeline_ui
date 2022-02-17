1d6c8f7e2614cbbb57476b67fae65a5c5f68b9a1
AJTDaedalus <aaron.j.tyler@gmail.com>
2022-02-16 18:40:47 -0600

    Swapped file where db is defined to simplify model import.

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -4,13 +4,15 @@ App initialization file
 '''
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from settings import DevelopmentSettings
-from .db import db
+
 
 def create_app(settings=DevelopmentSettings):
     app = Flask(__name__)
     app.config.from_object(settings)
 
+    db = SQLAlchemy(app)
     db.init_app(app)
 
     from app.home import home as home_bp
--- code/settings.py
+++ code/settings.py
@@ -13,7 +13,7 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 #Create development settings
 class DevelopmentSettings(Config):
