c4d2cf9f310a7065f5ac40e311baa87b8b795d3e
emshee13 <98117558+emshee13@users.noreply.github.com>
2022-02-17 00:06:09 -0500

    Add files via upload

--- code/app/models.py
+++ code/app/models.py
@@ -16,5 +16,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    
+    def set_password(self, password):
+        self.password_hash = password
+        
+    def check_password(self, password):
+        return self.password_hash
+        
     def __repr__(self):
         return '<User {}>'.format(self.username)
