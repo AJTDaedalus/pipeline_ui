9543ad44767309ea1858772052818298741bb7e0
Sarah <Sarahnsweet@gmail.com>
2022-02-21 13:39:43 -0800

    AddedFlashMessages

--- code/app/auth/views.py
+++ code/app/auth/views.py
@@ -1,3 +1,4 @@
+from unittest import TextTestResult
 from flask import render_template, redirect, url_for, flash, request
 from app.models import db
 from app.auth.forms import RegistrationForm
@@ -10,10 +11,27 @@ from flask_login import (login_user, login_required, current_user, logout_user)
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    password=form.password.data
+    SpecialSym="*?!'^+%&/()=}][{$#"
+    val=False
     if form.validate_on_submit():
         if User.query.filter_by(username=form.username.data.lower()).first():
             flash('User already exists.', 'error')
-            return redirect(url_for('home.index'))
+            return redirect(url_for('auth.register'))
+        if len(password) < 8:
+            flash('Password too short. Must be 8 characters','error')
+            return redirect(url_for('auth.register'))
+            val=TextTestResult
+        if not any (char.isupper() for char in password):
+            flash('Password must include at least one upper case letter','error')
+            return redirect(url_for('auth.register'))
+        if not any (char.isdigit() for char in password):
+            flash('Password must include at least one digit.','error')
+            return redirect(url_for('auth.register'))
+        if not any(char in SpecialSym for char in password):
+            flash('Password must include at least one special character.','error')
+            return redirect(url_for('auth.register'))   
+     
         else:
             user = User(username=form.username.data.lower(),
                         email=form.email.data)
@@ -23,6 +41,10 @@ def register():
             flash('Congratulations, you are now a registered user!', 'success')
             return redirect(url_for('home.index'))
     return render_template('registration.html', title='Register', form=form)
+
+
+
+
 
 #login route
 @auth.route('/login', methods=['GET', 'POST'])
