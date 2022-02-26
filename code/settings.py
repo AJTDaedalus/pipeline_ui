b32403b6ab2fc648f04738fd429cc2f58f1b2ea3
AJTDaedalus <aaron.j.tyler@gmail.com>
2022-02-26 14:37:19 -0600

    Connected mariadb/mysql

--- code/settings.py
+++ code/settings.py
@@ -18,6 +18,6 @@ class Config(object):
 #Create development settings
 class DevelopmentSettings(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'dev-data.db')
+    SQLALCHEMY_DATABASE_URI = 'mariadb+mysqlconnector://pipeline:pipeline123@db/pipeline_ui'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'Thisismykeyitisverysecret'
