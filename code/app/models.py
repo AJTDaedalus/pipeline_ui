fb58ac2d5cd9e070bb4860ef5bb1ac5bc0dc8205
LSMachuca <98370207+LSMachuca@users.noreply.github.com>
2022-03-17 21:33:26 -0400

    created display schema

--- code/app/models.py
+++ code/app/models.py
@@ -31,15 +31,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     def __repr__(self):
         return '<User {}>'.format(self.username)
-             
+
 
     # The home page should have 5 tabs, each capable of performing some type of request handling
     # the data for each tab will be stored in the RequestDetails tables
@@ -59,5 +59,11 @@ class RequestDetails(db.Model):
         self.userId = userId
         self.errorMessage = errorMessage
 
-
-
+class Display(db.Model):
+    JobID = db.Column(db.Integer, primary_key=True)
+    JobName = db.Column(db.String(255), unique=True, nullable=False)
+    DateSubmit = db.Column(db.DateTime, unique=False, nullable=False)
+    DataStart = db.Column(db.DateTime, unique=False, nullable=True)
+    DataEnd = db.Column(db.DateTime, unique=False, nullable=True)
+    Status = db.Column(db.String(255), unique=False, nullable=False)
+    UserID = db.Column(db.Integer, ForeignKey("User.id", ondelete="CASCADE"))
